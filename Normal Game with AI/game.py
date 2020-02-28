7feb6bccac444a604ea2483f8adb6614e82c4503
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2020-02-29 09:04:42 +1100

    Update game.py
    Fixed some bugs with the AI where it would sometimes override that humans turn. Also added a feature where the starting player is randomly selected

--- Normal Game with AI/game.py
+++ Normal Game with AI/game.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import sys, pygame, random
 pygame.init()
 
 size = width, height = 600, 600
@@ -31,9 +31,6 @@ def check_winning_states(player, grid):
         return True
     elif (grid[2][0] == player and grid[1][1] == player and grid[0][2] == player):
         return True
-    
-naughts = False
-crosses = True
 
 def map(p, a, b, c, d):
     q = ((p-a)*(d-c)//(b-a))+c
@@ -54,7 +51,12 @@ def aiMove(grid):
 
 running = True
 
+naughts = random.choice([True, False])
+crosses = not naughts
+print(naughts, crosses)
+
 while 1:
+    
     
     for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
@@ -76,8 +78,14 @@ while 1:
 
         # if crosses:
         #     aiMove(grid)
+        if crosses and running:
+            a = aiMove(grid)
+            grid[a[0]][a[1]] = ai
+            # grid[j][i] = ai
+            crosses = False
+            naughts = True
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and running:
             if event.button == 1:
                 (x,y) = pygame.mouse.get_pos()
                 i = map(x, 0, width, 0, 3)
@@ -88,19 +96,19 @@ while 1:
                         grid[j][i] = human
                         naughts = False
                         crosses = True
-                    elif crosses:
-                        a = aiMove(grid)
-                        grid[a[0]][a[1]] = ai
-                        # grid[j][i] = ai
-                        crosses = False
-                        naughts = True
+                    # elif crosses:
+                    #     a = aiMove(grid)
+                    #     grid[a[0]][a[1]] = ai
+                    #     # grid[j][i] = ai
+                    #     crosses = False
+                    #     naughts = True
 
-                for row in range(3):
-                    for col in range(3):
-                        if grid[row][col] == 'X':
-                            drawCrosses(int((width/3*col) + width/6), int((height/3*row) + height/6))
-                        elif grid[row][col] == 'O':
-                            drawNaughts(int((width/3*col) + width/6), int((height/3*row) + height/6))
+        for row in range(3):
+            for col in range(3):
+                if grid[row][col] == 'X':
+                    drawCrosses(int((width/3*col) + width/6), int((height/3*row) + height/6))
+                elif grid[row][col] == 'O':
+                    drawNaughts(int((width/3*col) + width/6), int((height/3*row) + height/6))
 
 
         #This call is needed to referesh the game screen and hence to display any changes that have been made to the display
